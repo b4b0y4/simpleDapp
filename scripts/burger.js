ecc736a750aec4c0327fe71e0f36ffcbc248ffd6
b4b0y4 <133552984+b4b0y4@users.noreply.github.com>
2024-02-13 11:00:37 +0900

    upgrade to the burger menu and minor fixes

--- scripts/burger.js
+++ scripts/burger.js
@@ -1,22 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const burgerMenuButton = document.querySelector('.burger-menu-button') // Selects the burger menu button element
-    const linksContainer = document.querySelector('.links-container') // Selects the links container element
-    const closeButton = document.querySelector('.close-button') // Selects the close button element
+    const burgerMenuButton = document.querySelector('.burger-menu-button')
+    const panel = document.getElementById('panel')
+    const closeButton = document.querySelector('.close-button')
+    const overlay = document.getElementById('overlay')
 
-    // Event listener for burger menu button click
     burgerMenuButton.addEventListener('click', function () {
-        linksContainer.classList.toggle('active') // Toggles the 'active' class on the links container
+        panel.classList.toggle('open')
+        overlay.style.display = "block"
     })
 
-    // Event listener for close button click
     closeButton.addEventListener('click', function () {
-        linksContainer.classList.remove('active'); // Removes the 'active' class from the links container
-    })
-
-    // Event listener for window resize
-    window.addEventListener('resize', function () {
-        if (window.innerWidth > 600) {
-            linksContainer.classList.remove('active') // Removes the 'active' class from the links container if window width is greater than 600px
-        }
+        panel.classList.remove('open')
+        overlay.style.display = "none"
     })
 })
